e868ec14572b49f8ededc891864f53d084aee73c
李林 <ww1095@163.com>
2016-11-08 09:13:27 +0800

    11

---
+++ app/src/main/java/com/example/liuwangshu/mytablayout/ListFragment.java
@@ -0,0 +1,35 @@
+package com.example.liuwangshu.mytablayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * author andli
+ * create at 16/11/8 上午8:46
+ * 列表Fragment
+ **/
+
+public class ListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mRecyclerView = (RecyclerView) inflater.inflate(R.layout.list_fragment, container, false);
+        return mRecyclerView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+        mRecyclerView.setAdapter(new RecyclerViewAdapter(getActivity()));
+    }
+}
